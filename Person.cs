c8d1aedfaedeafacdfab815551fead34ae90edda
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-05-07 18:49:01 -0500

    should actually work now
    Refactor AddHobbies method to append new hobbies to existing ones.

--- Person.cs
+++ Person.cs
@@ -1,20 +1,21 @@
-namespace knightmoves;
+﻿namespace knightmoves;
 
-public class Person
-{
-    public string FirstName;
-    public string LastName;
-    public string[] Hobbies;
+public class Person{
+   public string FirstName;
+   public string LastName;
+   public string[] Hobbies;
 
-    public Person(string firstName, string lastName)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-        Hobbies = new string[0];
-    }
+   public Person(string firstName, string lastName){
+      FirstName = firstName;
+      LastName = lastName;
 
-    public void AddHobbies(params string[] hobbies)
-    {
-        Hobbies = hobbies;       
-    }
+      Hobbies = new string[0];
+   }
+
+   public void AddHobbies(params string[] hobbies){
+      string[] newHobbies = new string[Hobbies.Length + hobbies.Length];
+      Hobbies.CopyTo(newHobbies, 0);
+      hobbies.CopyTo(newHobbies, Hobbies.Length);
+      Hobbies = newHobbies;
+   }
 }
