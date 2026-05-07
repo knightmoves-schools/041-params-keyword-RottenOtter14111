45258ffc77952e53969844ca73037a010f096f63
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-05-06 21:18:31 -0500

    work??
    Added initialization for Hobbies and a method to add hobbies.

--- Person.cs
+++ Person.cs
@@ -4,9 +4,15 @@ public class Person{
    public string FirstName;
    public string LastName;
    public string[] Hobbies;
+   
 
    public Person(string firstName, string lastName){
       FirstName = firstName;
       LastName = lastName;
+    Hobbies = new string[0];
    }
+     public void addHobbies(parms string[] hobbies)
+      {
+         Hobbies = hobbies;
+      }
 }
